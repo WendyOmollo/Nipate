516a25f18cdf85407ab1866d32cf231a0c3960d6
WendyOmollo <omollow280@gmail.com>
2019-10-05 14:15:19 +0300

    Add an intent to take to me to the list of artists

--- app/src/main/java/com/wendyomollo/nipate/MainActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/MainActivity.java
@@ -2,6 +2,7 @@ package com.wendyomollo.nipate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     @Override
     public void onClick(View view){
-
+    Intent intent = new Intent(this,ArtistsActivity.class);
+    startActivity(intent);
     }
 }
