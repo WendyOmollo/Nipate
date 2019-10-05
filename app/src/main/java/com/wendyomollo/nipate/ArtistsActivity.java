0943c799dd48ccfeaef0dc18a2fdcf4e1f118cec
WendyOmollo <omollow280@gmail.com>
2019-10-05 16:32:23 +0300

    Add an adapter

--- app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
@@ -2,10 +2,13 @@ package com.wendyomollo.nipate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ArtistsActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView mListView;
@@ -16,5 +19,10 @@ public class ArtistsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artists);
+        Intent intent = getIntent();
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists);
+        mListView.setAdapter(adapter);
     }
 }
