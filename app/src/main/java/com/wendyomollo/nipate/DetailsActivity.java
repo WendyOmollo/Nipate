cd1ac2961a7fe1a7eee0f4b84d338629e522a09f
WendyOmollo <omollow280@gmail.com>
2019-10-06 20:37:52 +0300

    Remove a bindview in the details activity class

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -12,11 +12,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetailsActivity extends DialogFragment {
-    @BindView(R.id.artistButtonView) Button mArtistDetails;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_details, container, false);
-        ButterKnife.bind(mArtistDetails);
+        Button mArtistDetails = (Button)rootView.findViewById(R.id.artistButtonView);
 
         mArtistDetails.setOnClickListener(new View.OnClickListener() {
             @Override
