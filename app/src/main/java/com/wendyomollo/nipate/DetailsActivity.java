8f19baf0abfa1383b736f14e8fe82e96b6459657
WendyOmollo <omollow280@gmail.com>
2019-10-06 20:19:25 +0300

    Extend dialog fragment

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -1,4 +1,18 @@
 package com.wendyomollo.nipate;
 
-public class DetailsActivity {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DetailsActivity extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_details, container, false);
+
+        return rootView;
+    }
+
 }
