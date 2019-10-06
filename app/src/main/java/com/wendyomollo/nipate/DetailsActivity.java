6ba1dfabc6ec496cabf32e2cf793c9084f3f60ad
WendyOmollo <omollow280@gmail.com>
2019-10-06 20:25:06 +0300

    Add a function that dismisses the activity that shows details of the artist

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -9,14 +9,21 @@ import android.widget.Button;
 import androidx.fragment.app.DialogFragment;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailsActivity extends DialogFragment {
     @BindView(R.id.artistButtonView) Button mArtistDetails;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_details, container, false);
+        ButterKnife.bind(mArtistDetails);
 
-
+        mArtistDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
 
         return rootView;
     }
