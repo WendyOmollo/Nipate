8ac31b8a39b5326530ae9750adce1d7c3ed04e8e
WendyOmollo <omollow280@gmail.com>
2019-10-06 20:23:45 +0300

    Add a butterknife extension to the class

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.DialogFragment;
 
+import butterknife.BindView;
+
 public class DetailsActivity extends DialogFragment {
+    @BindView(R.id.artistButtonView) Button mArtistDetails;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_details, container, false);
+
+
 
         return rootView;
     }
