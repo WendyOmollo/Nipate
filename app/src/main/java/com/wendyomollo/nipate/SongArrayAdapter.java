43e7200193bc828a9b449f15b9b6dc7ad60096d8
WendyOmollo <omollow280@gmail.com>
2019-10-06 22:17:59 +0300

    Add a constructor

--- app/src/main/java/com/wendyomollo/nipate/SongArrayAdapter.java
+++ app/src/main/java/com/wendyomollo/nipate/SongArrayAdapter.java
@@ -1,4 +1,19 @@
 package com.wendyomollo.nipate;
+import android.widget.ArrayAdapter;
 
-public class SongArrayAdapter {
+import android.content.Context;
+
+public class SongArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mArtists;
+    private String[] mSongs;
+
+    public SongArrayAdapter(Context mContext,int resource,String[] mArtists,String[] mSongs){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mArtists=mArtists;
+        this.mSongs=mSongs;
+    }
+
+
 }
