f56fddcd3eb4cd926a1f09b8e55e6aff83a6c576
WendyOmollo <omollow280@gmail.com>
2019-10-06 21:54:41 +0300

    Add a string of songs

--- app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
@@ -22,6 +22,9 @@ public class ArtistsActivity extends AppCompatActivity {
     private String [] artists = new String[] {"Kevo Simple Boy","Cute Boy","Samantha","Karesh","Opobae","Katumbo","Okonkwo",
             "Matoke man","BlankRoom","Rose Malaki","Ali Manxu","Chingweng"};
 
+    private String [] songs = new String[]{"Mihadarati","Mafisi","Pendo la Jesus","Urembo gharama","Sisi Wenyewe","Katumbo dance",
+    "Kisii life","Katastrophiii","Kanisa Letu","Vijana","Pastor Bonoko"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
 
@@ -34,7 +37,7 @@ public class ArtistsActivity extends AppCompatActivity {
         mLocationTextView.setText("In "+ location +" we have the following artists.Click on an artist for more details:");
 
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists,songs);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
