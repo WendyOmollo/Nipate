2b3eebaeb451933db6d2e6c5168cab46ddb15690
WendyOmollo <omollow280@gmail.com>
2019-10-07 10:56:56 +0300

    Add a rule dependency

--- app/src/androidTest/java/com/wendyomollo/nipate/MainActivityInstrumentation.java
+++ app/src/androidTest/java/com/wendyomollo/nipate/MainActivityInstrumentation.java
@@ -1,4 +1,34 @@
 package com.wendyomollo.nipate;
 
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class);git
+@LargeTest
+
 public class MainActivityInstrumentation {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText(){
+    onView(withId(R.id.editText)).perform(typeText("Kisumu"))
+            .check(matches(withText("Kisumu")));
+    }
+
 }
+
+
