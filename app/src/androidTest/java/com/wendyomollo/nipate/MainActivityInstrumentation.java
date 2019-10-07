797afd5ae1adb166e9dc633c25dd7ab64ceea201
WendyOmollo <omollow280@gmail.com>
2019-10-07 11:17:23 +0300

    Add a test that  confirms if the test being typed is correct

--- app/src/androidTest/java/com/wendyomollo/nipate/MainActivityInstrumentation.java
+++ app/src/androidTest/java/com/wendyomollo/nipate/MainActivityInstrumentation.java
@@ -1,20 +1,22 @@
 package com.wendyomollo.nipate;
 
 import androidx.test.filters.LargeTest;
+
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.rule.ActivityTestRule;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-@RunWith(AndroidJUnit4.class);git
+@RunWith(AndroidJUnit4.class)
 @LargeTest
 
 public class MainActivityInstrumentation {
@@ -28,7 +30,6 @@ public class MainActivityInstrumentation {
     onView(withId(R.id.editText)).perform(typeText("Kisumu"))
             .check(matches(withText("Kisumu")));
     }
-
 }
 
 
