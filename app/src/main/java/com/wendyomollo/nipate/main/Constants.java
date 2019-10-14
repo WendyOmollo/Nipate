aa878acfefc44a496fc597dabba354ae12cc21f1
WendyOmollo <omollow280@gmail.com>
2019-10-14 15:29:35 +0300

    Solve errors for the activity main file

--- app/src/main/java/com/wendyomollo/nipate/main/Constants.java
+++ app/src/main/java/com/wendyomollo/nipate/main/Constants.java
@@ -1,4 +1,7 @@
 package com.wendyomollo.nipate.main;
 
+import com.wendyomollo.nipate.BuildConfig;
+
 public class Constants {
+    public static final String YOUTUBE_BASE_URL ="https://www.youtube.com/";
 }
