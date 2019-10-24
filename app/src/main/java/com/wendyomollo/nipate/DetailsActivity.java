267f29c57405cb67fffcaf5d185295a2adfaee0d
WendyOmollo <omollow280@gmail.com>
2019-10-24 12:32:14 +0300

    Add playlist

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -32,7 +32,7 @@ public class DetailsActivity extends YouTubeBaseActivity {
         onInitializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("Wjrrgrvq1ew");
+                youTubePlayer.loadPlaylist("RDU5ond3urCS0");
                 youTubePlayer.play();
             }
 
