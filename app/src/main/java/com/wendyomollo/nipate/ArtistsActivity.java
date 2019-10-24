a10fd4c68e810c49f36e29f7f1e8b82593276a1f
WendyOmollo <omollow280@gmail.com>
2019-10-24 12:47:40 +0300

    Update code

--- app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/ArtistsActivity.java
@@ -19,7 +19,7 @@ public class ArtistsActivity extends Activity {
         @BindView(R.id.progressBar)
         ProgressBar mProgressBar;
 
-        private ImageView imageView1;
+        private ImageView imageView1,imageView2,imageView3,imageView4,imageView5,imageView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +28,68 @@ public class ArtistsActivity extends Activity {
         Intent intent = getIntent();
 
         imageView1 = findViewById(R.id.artist1);
+        imageView2 = findViewById(R.id.artist2);
+        imageView3 = findViewById(R.id.artist3);
+        imageView4 = findViewById(R.id.artist4);
+        imageView5 = findViewById(R.id.artist8);
+        imageView6 = findViewById(R.id.artist5);
+
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View v) {
-                Intent intent1 = new Intent(ArtistsActivity.this,DetailsActivity.class);
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent1);
-
+                finish();
             }
         });
 
-        }
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ArtistsActivity.this, DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+                finish();
+            }
+        });
+
+
+    }
 }
