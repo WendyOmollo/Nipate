e8d657ba6fbaf938bab86787d2212f4f2ab72036
WendyOmollo <omollow280@gmail.com>
2019-10-25 08:37:37 +0300

    Check if the branch has an apk file

--- app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
+++ app/src/main/java/com/wendyomollo/nipate/DetailsActivity.java
@@ -30,7 +30,7 @@ public class DetailsActivity extends YouTubeBaseActivity {
         onInitializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("RDU5ond3urCS0");
+                youTubePlayer.loadPlaylist("RDU5ond3urCS0");
                 youTubePlayer.play();
             }
 
