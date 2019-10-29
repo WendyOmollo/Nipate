9be99f2cf75d3c3136bd2b45950ff6cc3ed45b99
WendyOmollo <omollow280@gmail.com>
2019-10-29 21:30:30 +0300

    Create an artist class to save artists

---
+++ app/src/main/java/com/wendyomollo/nipate/Artist.java
@@ -0,0 +1,8 @@
+package com.wendyomollo.nipate;
+
+public class Artist {
+    private String user_id;
+    private String artist_name;
+    private String artist_email;
+    private String artist_password;
+}
