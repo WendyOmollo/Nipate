3b09214646f3eaf6872efbfddb4ef0e3d9f8ae50
WendyOmollo <omollow280@gmail.com>
2019-10-30 21:33:51 +0300

    Create a client class

--- app/src/main/java/com/wendyomollo/nipate/ux/Constants.java
+++ app/src/main/java/com/wendyomollo/nipate/ux/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public static final String key = BuildConfig.API_KEY;
 
     public static final String EVENTS_BASE_URL = "https://www.eventbriteapi.com/v3";
-    public static final String EVENTS_API_KEY = BuildConfig.EVENTS_API_KEY;
+    public static final String eventsApiKey = BuildConfig.EVENTS_API_KEY;
 }
--- app/src/main/java/com/wendyomollo/nipate/ux/EventsClient.java
+++ app/src/main/java/com/wendyomollo/nipate/ux/EventsClient.java
@@ -1,7 +1,45 @@
 package com.wendyomollo.nipate.ux;
 
+
+import com.wendyomollo.nipate.EventsApi;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.wendyomollo.nipate.ux.Constants.eventsApiKey;
+
 public class EventsClient {
 
+    private static Retrofit retrofit = null;
 
+    public static EventsApi getEventVenues() {
 
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("Authorization", eventsApiKey)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                        .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(eventsApiKey)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit.create(EventsApi.class);
+    }
 }
