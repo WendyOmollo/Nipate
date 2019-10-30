f8c6ec95239f67f336aa7c6f8e18a498db677f4b
WendyOmollo <omollow280@gmail.com>
2019-10-30 13:13:59 +0300

    Add an events api

--- app/src/main/java/com/wendyomollo/nipate/YoutubeApi.java
+++
@@ -1,8 +0,0 @@
-package com.wendyomollo.nipate;
-
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-
-public interface YoutubeApi {
-    public static final String YOUTUBE_BASE_URL ="https://www.googleapis.com/youtube/v3";
-}
