21c179138248793fc65b802e5cf45c746dd4c5e2
WendyOmollo <omollow280@gmail.com>
2019-10-30 13:31:42 +0300

    Incorporate the events api key in the project

--- app/src/main/java/com/wendyomollo/nipate/Constants.java
+++ app/src/main/java/com/wendyomollo/nipate/Constants.java
@@ -5,4 +5,7 @@ import com.wendyomollo.nipate.BuildConfig;
 public class Constants {
     public static final String YOUTUBE_BASE_URL ="https://www.googleapis.com/youtube/v3/";
     public static final String key = BuildConfig.API_KEY;
+
+    public static final String EVENTS_BASE_URL = "https://www.eventbriteapi.com/v3";
+    public static final String EVENTS_API_KEY = BuildConfig.EVENTS_API_KEY;
 }
